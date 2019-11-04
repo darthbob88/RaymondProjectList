a0eebbfa0ba3a4de1cb0be0ec11fd578e682abaf
Raymond Robert Price <darthbob88@gmail.com>
2019-11-04 13:12:38 -0800

    Fixed IdeaTable to pass the test

--- src/IdeaTable/IdeaTable.tsx
+++ src/IdeaTable/IdeaTable.tsx
@@ -5,18 +5,23 @@ type IdeaTableProps = {
     listOfIdeas: Idea[];
 }
 const IdeaTable: React.FunctionComponent<IdeaTableProps> = ({ listOfIdeas }) => {
-    return (<React.Fragment>
+    return (
+      <React.Fragment>
         <h1>List of Ideas</h1>
-        <ul id="ideaTable"> {
-            listOfIdeas.map(idea => <IdeaListing idea={idea} />)
-        }
-        </ul></React.Fragment>
-    )
+        {JSON.stringify(listOfIdeas)}
+        <ul id="ideaTable">
+          {" "}
+          {listOfIdeas.map(idea => (
+            <IdeaListing key={idea.slug} idea={idea} />
+          ))}
+        </ul>
+      </React.Fragment>
+    );
 }
 
 type IdeaListingProps = {idea: Idea};
 /**TODO: Better name. */
-const IdeaListing: React.FunctionComponent<IdeaListingProps> = ({idea}) => (<li key={idea.slug} className="ideaListing">
+const IdeaListing: React.FunctionComponent<IdeaListingProps> = ({idea}) => (<li  className="ideaListing">
     <h3>Summary: {idea.summary} </h3>
     <p>{idea.description}</p></li>)
 
