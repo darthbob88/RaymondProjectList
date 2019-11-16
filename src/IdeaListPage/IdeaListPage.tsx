84f61ecda9007093a2cde1fca766db0db62281ff
Raymond Robert Price <darthbob88@gmail.com>
2019-11-16 00:06:31 -0800

    Now I'm getting desperate

--- src/IdeaListPage/IdeaListPage.tsx
+++ src/IdeaListPage/IdeaListPage.tsx
@@ -3,6 +3,7 @@ import IdeaTable from "../IdeaTable/IdeaTable";
 import Idea from "../models/Idea";
 import NewIdeaForm from "../NewIdeaForm/NewIdeaForm";
 import * as IdeasService from "../models/IdeasService";
+import { databaseRef } from "../firebase";
 type IdeaListPageState = {
   listOfIdeas: Idea[];
   isLoading: boolean;
@@ -15,22 +16,46 @@ export default class IdeaListPage extends Component<{}, IdeaListPageState> {
       listOfIdeas: []
     };
   }
-  
+
   componentDidMount() {
-    IdeasService.fetchAllIdeas().then(result =>
-      this.setState({ listOfIdeas: result, isLoading: false })
-    ).catch(error => console.log(error));
+    //TODO: Turn this into a real-time watcher, so it gets automatically updated.
+    //TODO: Pull this into a separate ideasService
+    databaseRef
+      .collection("ideas")
+      .get()
+      .then(querySnapshot => {
+        // this.setState({ listOfIdeas: querySnapshot });
+        querySnapshot.forEach(doc => {
+          const newIdea = { ...doc.data(), id: doc.id } as Idea;
+          // console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+          this.setState({ listOfIdeas: [...this.state.listOfIdeas, newIdea] });
+        });
+      })
+      .catch(error => console.log(`Error fetching data: ${error}`));
+    // IdeasService.fetchAllIdeas().then(result =>
+    //   this.setState({ listOfIdeas: result, isLoading: false })
+    // ).catch(error => console.log(error));
   }
 
   addNewIdea = (newIdea: Idea) => {
     const oldIdeas = this.state.listOfIdeas;
-
-    IdeasService.addIdea(newIdea)
-      .then(result => {
-        const addedIdea = result.data() as Idea;
-        this.setState({ listOfIdeas: [...oldIdeas, addedIdea] });
+    databaseRef
+      .collection("ideas")
+      .add(newIdea)
+      .then(docRef => {
+        this.setState({
+          listOfIdeas: [...oldIdeas, newIdea]
+        });
       })
-      .catch(error => console.log(error));
+      .catch(function(error) {
+        console.error("Error adding document: ", error);
+      });
+    // IdeasService.addIdea(newIdea)
+    //   .then(result => {
+    //     const addedIdea = result.data() as Idea;
+    //     this.setState({ listOfIdeas: [...oldIdeas, addedIdea] });
+    //   })
+    //   .catch(error => console.log(error));
   };
 
   render() {
