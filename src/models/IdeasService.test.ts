0b8ea4b0685fd1b2cb7dba5a0e8f79d65fccfd96
Raymond Robert Price <darthbob88@gmail.com>
2019-11-16 20:04:52 -0800

    Adding tests for the IdeasService

---
+++ src/models/IdeasService.test.ts
@@ -0,0 +1,13 @@
+import * as IdeasService from "./IdeasService";
+import Idea, { otherNewIdea } from "./Idea";
+describe("IdeasService", () => {
+  it("should let me read data", async () => {
+    let listOfIdeas: Idea[] = await IdeasService.fetchAllIdeas();
+    expect(listOfIdeas.length).toBeGreaterThan(0);
+  });
+  it("should let me add data", async () => {
+    const ideaToAdd = otherNewIdea;
+    let addedIdea: Idea = await IdeasService.addIdea(ideaToAdd);
+    expect(addedIdea.description).toEqual(ideaToAdd.description);
+  });
+});
