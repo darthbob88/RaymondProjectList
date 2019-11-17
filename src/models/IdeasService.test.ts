d6a0acd4ddf583167e216fa53c698956bbd8328b
Raymond Robert Price <darthbob88@gmail.com>
2019-11-16 20:09:05 -0800

    Adding debug console.log

--- src/models/IdeasService.test.ts
+++ src/models/IdeasService.test.ts
@@ -3,6 +3,7 @@ import Idea, { otherNewIdea } from "./Idea";
 describe("IdeasService", () => {
   it("should let me read data", async () => {
     let listOfIdeas: Idea[] = await IdeasService.fetchAllIdeas();
+    console.log(listOfIdeas);
     expect(listOfIdeas.length).toBeGreaterThan(0);
   });
   it("should let me add data", async () => {
