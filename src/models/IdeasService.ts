c849a95c550e2731a60605e22003e5fb71d0dfa9
Raymond Robert Price <darthbob88@gmail.com>
2019-11-18 11:50:52 -0800

    Futzing with IdeasService

--- src/models/IdeasService.ts
+++ src/models/IdeasService.ts
@@ -2,18 +2,24 @@ import { databaseRef } from "../firebase";
 import Idea from "./Idea";
 
 //TODO: Turn this into a real-time watcher, so it gets automatically updated.
-export const fetchAllIdeas = async ():Promise<Idea[]> => {
+export const fetchAllIdeas = async (): Promise<Idea[]> => {
   let listOfIdeas: Idea[] = [];
   const querySnapshot = await databaseRef.collection("ideas").get();
-  querySnapshot.forEach(doc => {
+  listOfIdeas = querySnapshot.docs.map(doc => {
     const newIdea = { ...doc.data(), id: doc.id } as Idea;
-    listOfIdeas.unshift(newIdea);
-  }); 
+    return newIdea;
+  });
   return listOfIdeas;
 };
 //TODO: Should this return a single idea, or the entire updated list?
-export const addIdea = async (newIdea: Idea):Promise<Idea> => {
-  const docRef = await databaseRef.collection("ideas").add(newIdea);
- const actualDocument = await docRef.get();
-  return actualDocument.data() as Idea;
+export const addIdea = async (newIdea: Idea): Promise<Idea> => {
+  await databaseRef
+    .collection("ideas")
+    .doc(newIdea.slug)
+    .set(newIdea, { merge: true });
+  const addedIdea = await databaseRef
+    .collection("ideas")
+    .doc(newIdea.slug)
+    .get();
+  return addedIdea.data() as Idea;
 };
