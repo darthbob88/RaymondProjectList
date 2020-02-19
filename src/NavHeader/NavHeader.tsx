720133a08e3c82aea872b45d49f8e592ef307c1a
Raymond Price <darthbob88@gmail.com>
2020-02-19 14:54:43 -0800

    WIP

--- src/NavHeader/NavHeader.tsx
+++ src/NavHeader/NavHeader.tsx
@@ -1,6 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import * as ROUTES from "../routes"
-export const NavHeader = () => <React.Fragment><Link to={ROUTES.HOME}>Home</Link>
-<Link to={ROUTES.SIGN_IN}>Sign In</Link>
-<Link to={ROUTES.SIGN_UP}>Sign Up</Link></React.Fragment>
+import * as ROUTES from "../routes";
+import { FirebaseContext } from "../firebase";
+export const NavHeader = () => (
+  <FirebaseContext.Consumer>
+    {firebase => {
+      return (
+        <div>
+          <Link to={ROUTES.HOME}>Home</Link>
+          <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+          <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+          {firebase.currentUser != null
+            ? `Currently logged in as ${firebase.currentUser.email} `
+            : "Not currently logged in"}
+        </div>
+      );
+    }}
+  </FirebaseContext.Consumer>
+);
--- src/SignInForm/SignInForm.tsx
+++ src/SignInForm/SignInForm.tsx
@@ -1,17 +1,47 @@
 import React from "react";
 import { FirebaseContext } from "../firebase";
+//TODO: Replace this with a class/object, so I can just import AuthService
+import * as AuthService from "../models/AuthService";
+import * as ROUTES from "../routes";
 
-export const SignInForm = () => (
-  <div id="firebaseui-auth-container">
-    <FirebaseContext.Consumer>
-      {firebase => {
-        return (
-          <div>
-            I have access to Firebase and can render something. Current user is 
-            {firebase.currentUser && firebase.currentUser.email}
-          </div>
-        );
-      }}
-    </FirebaseContext.Consumer>
-  </div>
-);
+export const SignInForm = () => {
+  const onSubmit = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const INITIAL_STATE = {
+      username: "Raymond",
+      email: "darthbob88@gmail.com",
+      passwordOne: "lvader",
+      passwordTwo: "lvader",
+      error: null
+    };
+
+    AuthService.doSignInWithEmailAndPassword(
+      INITIAL_STATE.email,
+      INITIAL_STATE.passwordOne
+    )
+      .then(authUser => {
+        if (authUser.user != null)
+          console.log(`Successfully logged in as ${authUser.user.email}`);
+      })
+      .catch((error: any) => {});
+
+    event.preventDefault();
+  };
+  return (
+    <div id="firebaseui-auth-container">
+      <FirebaseContext.Consumer>
+        {firebase => {
+          return (
+            <div>
+              I have access to Firebase and can render something. Current user
+              is
+              {firebase.currentUser != null
+                ? firebase.currentUser.email
+                : "Nobody"}
+              <button onClick={onSubmit}>Log in as darthbob88</button>
+            </div>
+          );
+        }}
+      </FirebaseContext.Consumer>
+    </div>
+  );
+};
