70367fceb663e5cc6cfe8f700cc59dd3009959e2
Raymond Price <darthbob88@gmail.com>
2020-02-20 16:45:16 -0800

    PLEASE

--- src/NavHeader/NavHeader.tsx
+++ src/NavHeader/NavHeader.tsx
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import * as ROUTES from "../routes";
 import { FirebaseContext } from "../firebase";
-export const NavHeader = () => (
-  <FirebaseContext.Consumer>
-    {firebase => {
-      return (
-        <div>
-          <Link to={ROUTES.HOME}>Home</Link>
-          <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-          <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
-          {firebase.currentUser != null
-            ? `Currently logged in as ${firebase.currentUser.email} `
-            : "Not currently logged in"}
-        </div>
-      );
-    }}
-  </FirebaseContext.Consumer>
-);
+
+export const NavHeader = () => {
+  const FirebaseAuth = useContext(FirebaseContext);
+  let loggedIn ="Not currently logged in"
+   FirebaseAuth.onAuthStateChanged(user => {
+    loggedIn = user
+      ? `Currently logged in as ${user.email} `
+      : "Not currently logged in";
+  })
+  return (
+    <div>
+      <Link to={ROUTES.HOME}>Home</Link>
+      <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+      <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+      <span>{loggedIn}</span>
+    </div>
+  );
+};
+
+
