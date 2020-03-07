bb85474844df91ca5e7f2818ad4368ebcb5bb644
Raymond Price <darthbob88@gmail.com>
2020-03-06 19:46:41 -0800

    fix:Added logic to only show the NewIdeaForm for logged-in users.

--- src/NewIdeaForm/NewIdeaForm.tsx
+++ src/NewIdeaForm/NewIdeaForm.tsx
@@ -1,20 +1,25 @@
 import React, { Component } from "react";
 import { IdeaStatus, NewIdea } from "../models/Idea";
+import { AuthContext } from "../models/AuthService";
+import { ROUTES } from "../routes";
+import { Link } from "react-router-dom";
 interface NewIdeaProps {
   addNewIdea: (newIdea: NewIdea) => void;
 }
-
+//TODO: Replace with functional component
 export default class NewIdeaForm extends Component<NewIdeaProps, NewIdea> {
   constructor(props: NewIdeaProps) {
     super(props);
     this.state = {
       description: "",
       summary: "",
-      currentStatus:IdeaStatus.ToDo
+      currentStatus: IdeaStatus.ToDo
     };
   }
   //TODO: There has to be an overarching type for this.
-  handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     const target = event.currentTarget;
     const value = target.value;
     const name = target.name;
@@ -37,24 +42,47 @@ export default class NewIdeaForm extends Component<NewIdeaProps, NewIdea> {
   };
   render() {
     return (
-      <React.Fragment>
-        <form>
-          <h3>Adding New Idea</h3>
-          <label>
-            One-sentence summary of the idea
-            <input type="text" name="summary" onChange={this.handleChange} />
-          </label>
-          <label>
-            Long description of the idea
-            <textarea
-              rows={6}
-              name="description"
-              onChange={this.handleChange}
-            />
-          </label>
-          <button onClick={this.submitIdea}>Add new idea to list</button>
-        </form>
-      </React.Fragment>
+      <AuthContext.Consumer>
+        {authContext => {
+          return (
+            <form>
+              <h3>Adding New Idea</h3>
+              {authContext.currentUser == null ? (
+                <p>
+                  <Link to={ROUTES.SIGN_IN}>
+                    Please log in before submitting new ideas
+                  </Link>
+                </p>
+              ) : (
+                <React.Fragment>
+                  <label>
+                    One-sentence summary of the idea
+                    <input
+                      type="text"
+                      name="summary"
+                      onChange={this.handleChange}
+                    />
+                  </label>
+                  <label>
+                    Long description of the idea
+                    <textarea
+                      rows={6}
+                      name="description"
+                      onChange={this.handleChange}
+                    />
+                  </label>
+                </React.Fragment>
+              )}
+              <button
+                onClick={this.submitIdea}
+                disabled={authContext.currentUser == null}
+              >
+                Add new idea to list
+              </button>
+            </form>
+          );
+        }}
+      </AuthContext.Consumer>
     );
   }
 }
