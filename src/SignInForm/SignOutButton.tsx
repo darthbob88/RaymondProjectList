b1b5acf73ed6e744743a98c514f5a77c8d534090
Raymond Price <darthbob88@gmail.com>
2020-03-06 16:19:45 -0800

    fix: Changed SignOut to use AuthContext rather than AuthService directly.

--- src/SignInForm/SignOutButton.tsx
+++ src/SignInForm/SignOutButton.tsx
@@ -1,6 +1,8 @@
-import React from "react";
-import { AuthService} from "../models/AuthService";
+import React, { useContext } from "react";
+import { AuthContext} from "../models/AuthService";
 export const SignOutButton = () => {
+  
+  const AuthService = useContext(AuthContext);
   const signOut = () => {
     AuthService.doSignOut().then(() => {
         console.log(`Successfully signed out`)
