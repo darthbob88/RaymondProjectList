cee35456b9b439a8d2b61dddf5c9b01ad9f0f3bf
Raymond Price <darthbob88@gmail.com>
2020-03-15 13:38:02 -0700

    fix: Added margin-top on the NavFooter

--- src/NavFooter/NavFooter.tsx
+++ src/NavFooter/NavFooter.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import styles from "./NavFooter.module.css"
 
 export const NavFooter = () => {
-   return <footer>
+   return <footer className={styles.NavFooter}>
           &copy; {new Date().getFullYear()} Copyright: Raymond Price
           <Link to={"https://github.com/darthbob88/"}>Github</Link>
           <Link to="https://twitter.com/darthbob88">Twitter</Link>
