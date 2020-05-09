c5fc8e698c22a6ab741d5c4da1f3f611b43889a7
Raymond Price <darthbob88@users.noreply.github.com>
2020-05-08 23:32:13 -0700

    Update NavFooter.tsx

--- src/NavFooter/NavFooter.tsx
+++ src/NavFooter/NavFooter.tsx
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 export const NavFooter = () => {
    return <footer>
           &copy; {new Date().getFullYear()} Copyright: Raymond Price
-          <Link to={"https://github.com/darthbob88/"}>Github</Link>
-          <Link to="https://twitter.com/darthbob88">Twitter</Link>
-          <Link to="https://www.linkedin.com/in/raymond-price-10a3746b/">Linkedin</Link>
+          <a target="_blank" rel="noreferrer noopener" href="https://github.com/darthbob88/">Github</a>
+          <a target="_blank" rel="noreferrer noopener" href="https://twitter.com/darthbob88">Twitter</a>
+          <a target="_blank" rel="noreferrer noopener" href="https://www.linkedin.com/in/raymond-price-10a3746b/">Linkedin</a>
         </footer>
   };
